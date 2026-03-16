3a31e3b0f378ce7e3f71a3dfcfaf4c7421e87e98
백진선 <deck02@naver.com>
2026-03-16 19:21:56 +0900

    init: 프로젝트 초기 설정

---
+++ BE/promate/src/main/java/org/example/promate/PromateApplication.java
@@ -0,0 +1,13 @@
+package org.example.promate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromateApplication.class, args);
+    }
+
+}
---
+++ BE/promate/src/main/java/org/example/promate/user/entity/User.java
@@ -0,0 +1,4 @@
+package org.example.promate.user.entity;
+
+public class User {
+}

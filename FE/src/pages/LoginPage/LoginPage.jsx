28725b928f83e45ee97565e679d09ebf4b505637
구정아 <jeongkkt@gmail.com>
2026-05-01 02:06:50 +0900

    refactor: 카카오 로그인 URL 요청 방식 백엔드 API 연동으로 개선

--- FE/src/pages/LoginPage/LoginPage.jsx
+++ FE/src/pages/LoginPage/LoginPage.jsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import "./LoginPage.css";
 import LoginLogo from "./components/LoginLogo";
 import LoginForm from "./components/LoginForm";
-
-const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+import { getKakaoLoginUrl } from "../../apis/authApi";
 
 function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
@@ -16,21 +14,24 @@ function LoginPage() {
     return Array.from(array, (number) => number.toString(36)).join("");
   };
 
-  const kakaoHandleLogin = (keepLogin) => {
+  const kakaoHandleLogin = async (keepLogin) => {
     setIsLoading(true);
 
-    const url = new URL("https://kauth.kakao.com/oauth/authorize");
+    try {
+      const backendUrl = await getKakaoLoginUrl();
+      const url = new URL(backendUrl);
 
-    url.searchParams.append("client_id", KAKAO_CLIENT_ID);
-    url.searchParams.append("redirect_uri", REDIRECT_URI);
-    url.searchParams.append("response_type", "code");
+      const state = createOAuthState();
+      localStorage.setItem("oauth_state", state);
+      localStorage.setItem("keep_login", keepLogin);
+      url.searchParams.append("state", state);
 
-    const state = createOAuthState();
-    localStorage.setItem("oauth_state", state);
-    localStorage.setItem("keep_login", keepLogin);
-    url.searchParams.append("state", state);
-
-    window.location.href = url.toString();
+      window.location.href = url.toString();
+    } catch (error) {
+      console.error("로그인 URL 요청 실패:", error);
+      alert("로그인 서버와 연결할 수 없습니다. 잠시 후 다시 시도해주세요.");
+      setIsLoading(false);
+    }
   };
 
   return (

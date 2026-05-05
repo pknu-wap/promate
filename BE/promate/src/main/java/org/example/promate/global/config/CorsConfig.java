d1ad6b4438e0c636d0f7e19fcc1d5652929e978a
chj <qwer970901@naver.com>
2026-05-06 06:26:46 +0900

    fix: CorsConfig CORS 허용 설정 수정

--- BE/promate/src/main/java/org/example/promate/global/config/CorsConfig.java
+++ BE/promate/src/main/java/org/example/promate/global/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://promate-kappa.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);

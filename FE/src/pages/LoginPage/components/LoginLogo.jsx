e798454cd9040a7e58877e802e299498ecb0126e
구정아 <jeongkkt@gmail.com>
2026-05-05 19:11:02 +0900

    style: 로그인페이지 로고 변경

--- FE/src/pages/LoginPage/components/LoginLogo.jsx
+++ FE/src/pages/LoginPage/components/LoginLogo.jsx
@@ -1,11 +1,12 @@
-import promateLogo from '../../../assets/promate_logo.svg';
+import logoImg from '../../../assets/logoOrange.svg';
 
 function LoginLogo() {
   return (
     <header className="logo-section">
       <div className="logo-row">
         <div className="logo-wrapper">
-          <img src={promateLogo} alt="ProMate 로고" />
+          <img src={logoImg} alt="ProMate 로고" />
+          <span className="logo-title">PRO:MATE</span>
         </div>
       </div>
       <p className="service-subtitle">최고의 팀과 협업하세요.</p>

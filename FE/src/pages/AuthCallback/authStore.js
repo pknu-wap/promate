f44a7a77a9908e5680a051fde67dffbfcb4329c2
구정아 <jeongkkt@gmail.com>
2026-05-06 18:45:12 +0900

    Revert "[fix] 공통 - 로그인 후처리 오류 수정"

--- FE/src/pages/AuthCallback/authStore.js
+++ FE/src/pages/AuthCallback/authStore.js
@@ -2,14 +2,10 @@ import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
   isLoggedIn: !!localStorage.getItem("accessToken"),
-
   login: (accessToken) => {
-    if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
-      set({ isLoggedIn: true });
-    }
+    if (accessToken) localStorage.setItem("accessToken", accessToken);
+    set({ isLoggedIn: true });
   },
-
   logout: () => {
     localStorage.removeItem("accessToken");
     set({ isLoggedIn: false });

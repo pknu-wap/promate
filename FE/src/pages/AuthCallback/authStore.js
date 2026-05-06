7bc651ef876145b0af3183a6e66b7a6ee2d94aac
구정아 <jeongkkt@gmail.com>
2026-05-06 18:36:56 +0900

    fix: 오류 수정

--- FE/src/pages/AuthCallback/authStore.js
+++ FE/src/pages/AuthCallback/authStore.js
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
   isLoggedIn: !!localStorage.getItem("accessToken"),
+
   login: (accessToken) => {
-    if (accessToken) localStorage.setItem("accessToken", accessToken);
-    set({ isLoggedIn: true });
+    if (accessToken) {
+      localStorage.setItem("accessToken", accessToken);
+      set({ isLoggedIn: true });
+    }
   },
+
   logout: () => {
     localStorage.removeItem("accessToken");
     set({ isLoggedIn: false });

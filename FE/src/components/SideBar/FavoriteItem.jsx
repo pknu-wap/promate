b4c9ab730a9a77e75283410e791378277eb2bb5f
구정아 <jeongkkt@gmail.com>
2026-05-14 04:00:21 +0900

    style: 진행중인 프젝에 호버 효과 추가

--- FE/src/components/SideBar/FavoriteItem.jsx
+++ FE/src/components/SideBar/FavoriteItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function FavoriteItem({ id, name, dueDate, dotColor, onClick }) {
   return (
     <li className="favorite-list-item">
-      <Link to={`/projects/${id}`} style={{ textDecoration: 'none', color: 'inherit', display: 'block' }} onClick={onClick}>
+      <Link to={`/projects/${id}`} className="favorite-item-link" onClick={onClick}>
         <div className="favorite-item-header">
           <span className={`dot ${dotColor}`}></span> 
           {name}

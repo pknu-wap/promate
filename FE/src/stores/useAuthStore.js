9f27e05a8daee073b4e6b3433c7e4c3eef7c454f
구정아 <jeongkkt@gmail.com>
2026-06-04 12:25:18 +0900

    refactor: 카카오 로그인 요청 파라미터 생성 로직 개선

--- FE/src/stores/useAuthStore.js
+++ FE/src/stores/useAuthStore.js
@@ -1,7 +1,12 @@
 import { create } from "zustand";
 
+const getInitialLoginState = () => {
+  const token = localStorage.getItem("accessToken");
+  return !!token && token !== "null" && token !== "undefined";
+};
+
 export const useAuthStore = create((set) => ({
-  isLoggedIn: !!localStorage.getItem("accessToken"),
+  isLoggedIn: getInitialLoginState(),
   login: (accessToken, refreshToken) => {
     if (accessToken) localStorage.setItem("accessToken", accessToken);
     if (refreshToken) localStorage.setItem("refreshToken", refreshToken);
